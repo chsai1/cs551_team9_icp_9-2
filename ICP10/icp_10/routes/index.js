ceed5e80f92db36423b80a3a2db89b578576d8af
saidivyachennupati <42659823+saidivyachennupati@users.noreply.github.com>
2018-11-15 13:30:20 -0600

    lab 3 updated

--- ICP10/icp_10/routes/index.js
+++ ICP10/icp_10/routes/index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var searchtext = req.query.searchtext;
-  var url = "https://kgsearch.googleapis.com/v1/entities:search?query="+searchtext+"&key=%20AIzaSyDMEBecZDKiws3eLRFIi1IO1E6Vm9s4Xj8%20&limit=1&indent=True";
+  var url = "https://api.nutritionix.com/v1_1/search/'+$scope.dish+'?results=0:1&fields=*&appId=50cd56b2&appKey=bb1554e78ba472b88ab75048dd8f2fce";
   request.get(url).pipe(res);
 });
 
